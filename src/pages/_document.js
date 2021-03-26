ac06a39e69dcfd4704fa3447084f011383baad19
Cem Nisan <2017417057@ogr.cu.edu.tr>
2021-03-26 15:14:10 +0300

    google tag manager added.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import {GA_TRACKING_ID} from '../../src/utilities/gtag';
+import { GA_TRACKING_ID } from "../../src/utilities/gtag";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -11,6 +11,23 @@ class MyDocument extends Document {
     return (
       <Html lang="tr">
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-TWTRT23');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TWTRT23" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
@@ -26,7 +43,6 @@ class MyDocument extends Document {
           `,
             }}
           />
-
         </Head>
         <body>
           <Main />
